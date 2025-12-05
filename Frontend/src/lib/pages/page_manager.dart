e55c4abc7ec237af94bec1c2a459c8beb4c33d05
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2025-12-05 13:41:28 +0100

    Decifratura di un token JWT di prova

--- Frontend/src/lib/pages/page_manager.dart
+++ Frontend/src/lib/pages/page_manager.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:provider/provider.dart';
 import 'package:test_app/navbar/navbar.dart';
+
+// Aggiunta libreria per la decodifica del token JWT
+import 'package:jwt_decoder/jwt_decoder.dart';
+
 
 class PageManager extends StatelessWidget {  
   const PageManager({super.key});
@@ -14,6 +19,53 @@ class PageManager extends StatelessWidget {
           return Column(
             children: [
               const Navbar(),
+              /* INIZIO SEZIONE DI PROVA PER DECIFARE UN TOKEN JWT*/ 
+              Row(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey,
+                        foregroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+
+                        /*
+                        Token utilizzato per il test:
+                        {
+                          "ID": "23145",
+                          "Utente": "Franco Ricciardi",
+                          "Titolo": "Bug rilevato sul progetto n° 243",
+                          "Descrizione": "Una volta eseguito l' accesso, l' app si chiude",
+                          "Priorità": "Alta",
+                          "Stato": "In progress",
+                          "Tipo": "Bug"
+                        }
+                        */ 
+
+                        // Token codificato (Algorithm: HS256 , Key: Centosessantasette)
+
+                        String token = "eyJhbGciOiJIUzI1NiJ9.eyJUaXBvIjoiQnVnIiwiVGl0b2xvIjoiQnVnIHJpbGV2YXRvIHN1bCBwcm9nZXR0byBuwrAgMjQzIiwiVXRlbnRlIjoiRnJhbmNvIFJpY2NpYXJkaSIsIkRlc2NyaXppb25lIjoiVW5hIHZvbHRhIGVzZWd1aXRvIGwnIGFjY2Vzc28sIGwnIGFwcCBzaSBjaGl1ZGUiLCJQcmlvcml0w6AiOiJBbHRhIiwiSUQiOiIyMzE0NSIsIlN0YXRvIjoiSW4gcHJvZ3Jlc3MifQ.5pE4piH8iq6KKrsC315Cu0VVqEh3ICiyPyZ2RUUjw9s";
+
+                        Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+
+                        // Stampa dell' intero token decodificato e di alcuni campi specifici
+                        
+                        print("JWT Token decodificato: $decodedToken");
+
+                        print("ID: ${decodedToken['ID']}");
+
+                        print("Priorità: ${decodedToken['Priorità']}");
+
+                      },
+                      child: Text("Decodifica JWT Token ORA (Vedi risultato nella debug console)"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            /* FINE SEZIONE DI PROVA PER DECIFARE UN TOKEN JWT*/ 
               Expanded(
                 child: state.selectedPage
               )
