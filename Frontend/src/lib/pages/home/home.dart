892352ddcbc17adbe7391dd434b5849d6d56352a
RiuHz <ditotagaetano@outlook.it>
2025-12-26 18:17:03 +0100

    Added home and json fetch from url

--- Frontend/src/lib/pages/home/home.dart
+++ Frontend/src/lib/pages/home/home.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
 import 'package:test_app/classes/issue/issue.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-
-class HomePage extends StatelessWidget {
-  final List<Issue> issues = get; // Questa dovrà diventare una richiesta HTTPS ai servizi amazon aws 
-
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  late Future<List<Issue>> issues = fetchIssues();
+
+  @override
+  void initState() {super.initState();}
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Text('Filtri'),
-        ListView.builder(
-          itemCount: issues.length,
-          prototypeItem: issues.first,
-          itemBuilder: (context, index) {
-            return issues[index];
-          },
+        FutureBuilder<List<Issue>> (
+          future: issues,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('OPS... Qualcosa è andato storto');
+            } else if (snapshot.hasData) {
+              return Expanded(
+                child: buildIssuesList(snapshot.data!)
+              );
+            } else {
+              return const Text('Non sono presenti Issue al momento!');
+            }
+          }
         )
       ],
     );
   }
+
+  Widget buildIssuesList(List<Issue> issues) => ListView.builder(
+    itemCount: issues.length,
+    itemBuilder: (context, index) {
+      final issue = issues[index];
+
+      return Issue.toCard(context, issue);
+    }
+  );
 }
 
 List<Issue> parseIssues(String responseBody) {
@@ -33,18 +59,15 @@ List<Issue> parseIssues(String responseBody) {
   return parsed.map<Issue>(Issue.fromJson).toList();
 }
 
-Future<List<Issue>> fetchIssues(http.Client client) async {
-/* COSI SI FA LA RICHIESTA HTTP
-  final response = await client.get(
-    Uri.parse('https://jsonplaceholder.typicode.com/photos'),
+Future<List<Issue>> fetchIssues() async {
+  // Cambiare poi url ai servizi AWS
+  final response = await http.get(
+    Uri.parse('https://raw.githubusercontent.com/RiuHz/UniNaBugBoard26/refs/heads/main/Frontend/src/lib/pages/home/data.json'),
   );
 
-  return compute(parseIssues, response.body);  
-*/ 
-
-  // Questa invece è solo una lettura da disco in fase di sviluppo
-  
-
-
-
+  if (response.statusCode == 200) {
+    return compute(parseIssues, response.body);
+  } else {
+    throw Exception('Failed to load issues from server...');
+  }
 }
