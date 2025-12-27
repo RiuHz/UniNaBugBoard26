d72e4139fb8d463dc6c78ba0b21d9737aeedb020
RiuHz <ditotagaetano@outlook.it>
2025-12-27 13:46:32 +0100

    Modificata graficamente la IssueCard e suddivisa in componenti
    Co-authored-by: eman-giaquinto eman.giaquinto@studenti.unina.it

--- Frontend/src/lib/pages/home/issue_card.dart
+++ Frontend/src/lib/pages/home/issue_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/classes/issues/issue.dart';
+import 'package:test_app/classes/user/user.dart';
+import 'package:test_app/theme/theme.dart';
 
 class IssueCard extends StatelessWidget {
   final Issue issue;
@@ -9,12 +11,17 @@ class IssueCard extends StatelessWidget {
     required this.issue
   });
 
-  // Fa schifo sta roba...
-
   @override
   Widget build(BuildContext context) {
     return Card(
       surfaceTintColor: Theme.of(context).colorScheme.inverseSurface,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25),
+        side: BorderSide(
+          color: selectColorFromPriority(context, issue.priority),
+          width: 2.0
+        )
+      ),
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
         child: SizedBox(
@@ -22,56 +29,16 @@ class IssueCard extends StatelessWidget {
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              AspectRatio(aspectRatio: 1.0, child: issue.icon),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        issue.title,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      const Padding(padding: EdgeInsets.only(bottom: 2.0)),
-                      Expanded(
-                        child: Text(
-                          issue.description,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(fontSize: 12.0,),
-                        ),
-                      ),
-                      Text(
-                        '${issue.user.surname} ${issue.user.name}',
-                        style: const TextStyle(fontSize: 12.0)
-                      ),
-                    ],
-                  )
-                ),
+              IssueCardIcon(icon: issue.icon),
+              IssueCardInfo(
+                title: issue.title,
+                description: issue.description,
+                user: issue.user
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Stato: ${issue.state}',
-                      style: const TextStyle(fontSize: 12.0)
-                    ),
-                    const Padding(padding: EdgeInsets.only(bottom: 2.0)),
-                    Expanded(
-                      child: Text(
-                        'Priorità: ${issue.priority}',
-                        style: const TextStyle(fontSize: 12.0)
-                      ),
-                    ),
-                    Text('Bottone')
-                  ],
-                )
+              IssueCardState(
+                state: issue.state,
+                priority: issue.priority,
+                isEditable: issue.isEditable(context)
               )
             ],
           ),
@@ -80,4 +47,128 @@ class IssueCard extends StatelessWidget {
     );
   }
 
+  Color selectColorFromPriority(BuildContext context, String priority) {
+    switch (priority) {
+      case 'Low':
+        return Theme.of(context).green;
+      case 'Medium':
+        return Theme.of(context).yellow;
+      case 'High':
+        return Theme.of(context).red;
+    }
+
+    throw Exception('Unkown priority for this issue...');
+  }
+
+}
+
+class IssueCardIcon extends StatelessWidget {
+  final Icon icon;
+
+  const IssueCardIcon({
+    super.key,
+    required this.icon
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(aspectRatio: 1.0, child: icon);
+  }
+}
+
+class IssueCardInfo extends StatelessWidget {
+  final String title;
+  final String description;
+  final User user;
+
+  const IssueCardInfo({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.user
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+            Expanded(
+              child: Text(
+                description,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontSize: 12.0,),
+              ),
+            ),
+            Text(
+              '${user.surname} ${user.name}',
+              style: const TextStyle(fontSize: 12.0)
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
+
+class IssueCardState extends StatelessWidget {
+  final String state;
+  final String priority;
+  final bool isEditable;
+
+  const IssueCardState({
+    super.key,
+    required this.state,
+    required this.priority,
+    required this.isEditable
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+      child: SizedBox(
+        width: 125,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Stato: $state',
+              style: const TextStyle(fontSize: 12.0)
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+            Expanded(
+              child: Text(
+                'Priorità: $priority',
+                style: const TextStyle(fontSize: 12.0)
+              ),
+            ),
+            if (isEditable)
+              ElevatedButton(
+                onPressed: () {}, // Questa sarà la funzione per il display della pagina di modifica
+                style: ElevatedButton.styleFrom(
+                  shadowColor: Theme.of(context).colorScheme.inverseSurface,
+                ),
+                child: Text(
+                  'Modifica',
+                  style: TextStyle(color: Theme.of(context).colorScheme.inverseSurface)
+                ),
+              ),
+          ],
+        )
+      )
+    );
+  }
 }
