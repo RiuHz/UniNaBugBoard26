d97f656232143c05237367b5ffd149cdf3b44487
RiuHz <ditotagaetano@outlook.it>
2025-12-27 13:45:45 +0100

    Aggiunti i campi 'id' e 'role' alla classe utente
    Co-authored-by: eman-giaquinto eman.giaquinto@studenti.unina.it

--- Frontend/src/lib/classes/user/user.dart
+++ Frontend/src/lib/classes/user/user.dart
@@ -1,14 +1,20 @@
 class User {
+  final String id;
   final String name;
   final String surname;
+  final String role;
 
   const User({
+    required this.id,
     required this.name,
-    required this.surname
+    required this.surname,
+    required this.role,
   });
 
   static User fromJson(Map<String, dynamic> json) => User(
-      name: json['Name'] as String,
-      surname: json['Surname'] as String
+      id: json['Id'] as String? ?? '',
+      name: json['Name'] as String? ?? '',
+      surname: json['Surname'] as String? ?? '',
+      role: json['Role'] as String? ?? ''
     );
 }
