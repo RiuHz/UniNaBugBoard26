f7fea0ced637bb0462dbba8eac4a5a2ebf5cf3b7
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2025-12-29 17:36:17 +0100

    Creazione classe issue

---
+++ Backend/src/main/java/com/progetto/dto/Issue.java
@@ -0,0 +1,84 @@
+package com.progetto.dto;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.progetto.enums.*;
+
+// Definisco l'ordine delle proprietà dell' oggetto JSON restituito al front-end
+@JsonPropertyOrder({"id", "userId", "titolo", "descrizione", "tipo", "priorita", "stato", "allegato"}) 
+public class Issue {
+    private int id;
+    private String userId;
+    private String titolo;
+    private String descrizione;
+    private Tipo tipo;
+    private Priorita priorita;
+    private Stato stato;
+    private String allegato;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public Tipo getTipo(){
+        return tipo;
+    }
+
+    public Priorita getPriorita(){
+        return priorita;
+    }
+
+    public Stato getStato(){
+        return stato;
+    }
+
+    public String getAllegato(){
+        return allegato;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setPriorita(Priorita priorita) {
+        this.priorita = priorita;
+    }
+
+    public void setStato(Stato stato) {
+        this.stato = stato;
+    }
+
+    public void setAllegato(String allegato) {
+        this.allegato = allegato;
+    }
+
+    
+
+}
