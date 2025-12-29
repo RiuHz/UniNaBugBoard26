d5fe26e5702c4adff8fa7987737d7108ea5fa994
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2025-12-29 14:05:55 +0100

    Test richiesta in locale

---
+++ Backend/src/main/java/com/progetto/HomeController.java
@@ -0,0 +1,13 @@
+package com.progetto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/* L' annotazione controller serve per indicare che questa classe viene usate come web controller per ricevere richieste web */
+@Controller
+public class HomeController {
+    @RequestMapping("/") // mappa la richiesta alla radice del sito (es: /issues/allegato, e cosi via)
+    public String index(){
+        return "index.html";
+    }
+}
