396cc75d85d9ff169a8d1cbb59d9f619e2b27da3
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2025-12-30 00:24:01 +0100

    Classe che gestisce la logica di business per le issue

---
+++ Backend/src/main/java/com/progetto/service/IssueService.java
@@ -0,0 +1,31 @@
+package com.progetto.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.progetto.model.Issue;
+import com.progetto.repository.IssueRepository;
+
+// Il tag Service indica che questa classe fornisce servizi di logica di business
+@Service 
+public class IssueService {
+
+    private IssueRepository issueRepository;
+    
+    public IssueService(IssueRepository issueRepository) {
+        this.issueRepository = issueRepository;
+    }
+
+    // 1. Recupera tutti i dati
+    public List<Issue> recuperaTutteLeIssues() {
+        return issueRepository.findAll();
+    }
+
+    // 2. Recupera un solo dato in base all id che gli passiamo
+    public Issue recuperaIssuePerId(Integer id) {
+        return issueRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Issue non trovata con ID: " + id));
+    }
+
+}
