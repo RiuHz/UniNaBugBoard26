7f30f449c36744863f7156e6071582bcd45c50ef
RiuHz <ditotagaetano@outlook.it>
2025-12-30 19:45:01 +0100

    Aggiunta la classe SignUpRequest

---
+++ Frontend/src/lib/classes/sign up request/sign_up_request.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:test_app/enum/user/user_role.dart';
+
+class SignUpRequest {
+  final String email;
+  final String password;
+  final String name;
+  final String surname;
+  final UserRole role;
+
+  const SignUpRequest(
+    this.email,
+    this.password,
+    this.name,
+    this.surname,
+    this.role
+  );
+
+  String getJsonData() => json.encode(
+    {
+        "email": email,
+        "password": password,
+        "nome":  name,
+        "cognome": surname,
+        "ruolo": role.type,
+    }
+  );
+}
