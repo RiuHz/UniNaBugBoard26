67e8a175cdb83f146f229064b0a0b3fb51fb1853
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2025-12-30 20:30:05 +0100

    Realizzato il service per la richiesta di inserimento di un immagine

--- Backend/src/main/java/com/progetto/service/S3Service.java
+++ Backend/src/main/java/com/progetto/service/S3Service.java
@@ -1,13 +1,19 @@
 package com.progetto.service;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 public class S3Service {
@@ -31,5 +37,28 @@ public class S3Service {
 
         ResponseBytes<GetObjectResponse> objectBytes = s3Client.getObjectAsBytes(getObjectRequest);
         return objectBytes.asByteArray();
+    }
+
+    // Metodo per caricare l' immagine
+    public String uploadFile(MultipartFile file) throws IOException {
+
+    //Genero un nome univoco per evitare di sovrascrivere file esistenti
+    String originalFileName = file.getOriginalFilename();
+    String keyName = UUID.randomUUID().toString() + "_" + originalFileName;
+
+    // Creo la richiesta di upload
+    PutObjectRequest putOb = PutObjectRequest.builder()
+            .bucket(bucketName)
+            .key(keyName)
+            .contentType(file.getContentType())
+            .build();
+
+    // Invio il file ad S3
+    s3Client.putObject(putOb, RequestBody.fromBytes(file.getBytes()));
+    
+    //s3Client.putObject(putOb, RequestBody.fromInputStream(file.getInputStream(), file.getSize())); //Alternativa non usa ram
+
+    return keyName;
+    
     }
 }
