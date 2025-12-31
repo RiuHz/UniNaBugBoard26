e8a8ac3660a4688e14807fb5a2cd03a321df22db
RiuHz <ditotagaetano@outlook.it>
2025-12-31 14:32:36 +0100

    Eliminata la abstract class Issue

--- Backend/src/main/java/com/progetto/model/issues/StorageIssue.java
+++ Backend/src/main/java/com/progetto/model/issues/StorageIssue.java
@@ -6,18 +6,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @JsonPropertyOrder({"id", "userid", "titolo", "descrizione", "tipo", "priorita", "stato", "allegato"})
-@Entity 
-public class StorageIssue extends Issue {
-    private String allegato;
+@Table(name = "issue")
+@Entity
+public class StorageIssue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     
-    public String getallegato(){
-        return allegato;
-    }
+    private String userid;
+    private String titolo;
+    private String descrizione;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", columnDefinition = "tipo")
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private Tipo tipo;
 
-    public void setallegato(String allegato) {
-        this.allegato = allegato;
-    }
-    
+    @Enumerated(EnumType.STRING)
+    @Column(name = "priorita", columnDefinition = "priorita")
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private Priorita priorita;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "stato", columnDefinition = "stato")
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    private Stato stato;
+
+    private String allegato;
+        
     public static StorageIssue fromUserIssue(UserIssue userIssue) {
     	StorageIssue storageIssue = new StorageIssue();
     	
@@ -30,4 +46,59 @@ public class StorageIssue extends Issue {
     	return storageIssue;
     }
     
+    public String getallegato(){
+        return allegato;
+    }
+
+    public void setallegato(String allegato) {
+        this.allegato = allegato;
+    }
+
+    public String getuserid() {
+        return userid;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public Tipo getTipo(){
+        return tipo;
+    }
+
+    public Priorita getPriorita(){
+        return priorita;
+    }
+
+    public Stato getStato(){
+        return stato;
+    }
+
+    public void setuserid(String userid) {
+        this.userid = userid;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setPriorita(Priorita priorita) {
+        this.priorita = priorita;
+    }
+
+    public void setStato(Stato stato) {
+        this.stato = stato;
+    }
 }
--- Backend/src/main/java/com/progetto/model/issues/UserIssue.java
+++ Backend/src/main/java/com/progetto/model/issues/UserIssue.java
@@ -2,8 +2,62 @@ package com.progetto.model.issues;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public class UserIssue extends Issue {
+public class UserIssue {
+	private String userid;
+    private String titolo;
+    private String descrizione;
+	private Tipo tipo;
+	private Priorita priorita;
+	private Stato stato;
 	private MultipartFile immagine;
+
+	public String getuserid() {
+        return userid;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public Tipo getTipo(){
+        return tipo;
+    }
+
+    public Priorita getPriorita(){
+        return priorita;
+    }
+
+    public Stato getStato(){
+        return stato;
+    }
+
+    public void setuserid(String userid) {
+        this.userid = userid;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setPriorita(Priorita priorita) {
+        this.priorita = priorita;
+    }
+
+    public void setStato(Stato stato) {
+        this.stato = stato;
+    }
 	
 	public void setImmagine(MultipartFile immagine) {
 		this.immagine = immagine;
