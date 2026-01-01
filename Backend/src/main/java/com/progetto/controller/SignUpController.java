87f4b5a2879ba98dc5d14a26002754654bf01528
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-01-01 17:45:05 +0100

    Aggiunti import necessari

--- Backend/src/main/java/com/progetto/controller/SignUpController.java
+++ Backend/src/main/java/com/progetto/controller/SignUpController.java
@@ -1,13 +1,13 @@
 package com.progetto.controller;
 
-import com.progetto.model.RichiestaRegistrazione;
-import com.progetto.service.SignUpService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.progetto.model.RichiestaRegistrazione;
+import com.progetto.service.SignUpService;
 
 @RestController
 @RequestMapping("/sign-up")
