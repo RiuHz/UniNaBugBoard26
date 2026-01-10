6d4bb194aff9b8c5116ac837aaf662959d24491b
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-01-10 14:23:31 +0100

    Cambiato il nome della classe

--- Backend/src/main/java/com/progetto/specification/IssueSpecification.java
+++ Backend/src/main/java/com/progetto/specification/IssueSpecification.java
@@ -1,25 +1,24 @@
 package com.progetto.specification;
 
-import jakarta.persistence.criteria.Predicate;
-
 import org.springframework.data.jpa.domain.Specification;
 
-import com.progetto.enums.issue.*;
+import com.progetto.enums.issue.Priorita;
+import com.progetto.enums.issue.Stato;
+import com.progetto.enums.issue.Tipo;
 import com.progetto.models.Issue;
-import com.progetto.models.Utente;
 
-public class StorageIssueSpecification {
+import jakarta.persistence.criteria.Predicate;
 
-    private StorageIssueSpecification() {}
+public class IssueSpecification {
 
-    public static Specification<Issue> filtraIssue(Priorita priorita, Stato stato, Tipo tipo, Utente utente) {
+    public static Specification<Issue> filtraIssue(Priorita priorita, Stato stato, Tipo tipo, String utenteId) {
 
         return (root, query, criteriaBuilder) -> {
 
             Predicate filtri = criteriaBuilder.conjunction();
 
-            if (utente.getId() != null) {
-                criteriaBuilder.equal(root.get("utente"), utente.getId());
+            if (utenteId != null) {
+                filtri = criteriaBuilder.and(filtri, criteriaBuilder.equal(root.get("utente"), utenteId));
             }
 
             if (priorita != null) {
