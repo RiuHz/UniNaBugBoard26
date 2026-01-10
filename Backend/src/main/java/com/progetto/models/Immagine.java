f07cc06a3c5c849c029cf69dd60cc37d1f51c231
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-01-10 14:20:11 +0100

    Modificata la clsse dedicata all' allegato

--- Backend/src/main/java/com/progetto/models/Immagine.java
+++ Backend/src/main/java/com/progetto/models/Immagine.java
@@ -2,13 +2,8 @@ package com.progetto.models;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.Transient;
-
-@Embeddable
 public class Immagine {
     private String url;
-    @Transient
     private MultipartFile file;
 
     public String getUrl() {
