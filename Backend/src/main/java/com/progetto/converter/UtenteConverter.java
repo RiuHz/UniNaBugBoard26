56279fec9b0b0dd120eede5fc33821ec40088396
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-01-10 14:19:47 +0100

    Aggiunto il conveter per l' utente

---
+++ Backend/src/main/java/com/progetto/converter/UtenteConverter.java
@@ -0,0 +1,23 @@
+package com.progetto.converter;
+
+import com.progetto.models.Utente;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = false)
+public class UtenteConverter implements AttributeConverter<Utente, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Utente utente) {
+        return utente != null ? utente.getId() : null;
+    }
+
+    @Override
+    public Utente convertToEntityAttribute(String id) {
+        if(id == null) return null;
+        Utente utente = new Utente();
+        utente.setId(id);
+        return utente;
+    }
+}
