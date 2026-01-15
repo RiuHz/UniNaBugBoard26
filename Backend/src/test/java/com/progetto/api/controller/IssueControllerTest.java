90bb55ce94f4b4ffbb16c22d979a8abc2e539579
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-01-15 14:03:03 +0100

    Ottimizzazione per i test nella creazione di Issue

--- Backend/src/test/java/com/progetto/api/controller/IssueControllerTest.java
+++ Backend/src/test/java/com/progetto/api/controller/IssueControllerTest.java
@@ -1,22 +1,34 @@
 package com.progetto.api.controller;
 
 import java.util.Collections;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.mockito.BDDMockito.given;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.progetto.api.service.IssueService;
 import com.progetto.enums.issue.Priorita;
 import com.progetto.enums.issue.Stato;
 import com.progetto.enums.issue.Tipo;
+import com.progetto.models.Issue;
 import com.progetto.models.Utente;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.BDDMockito.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.nullValue;
+
 
 // Questa annotazione carica SOLO il controller IssueController, rendendo il test veloce (da cambiare sicuramente)
 @WebMvcTest(IssueController.class)
@@ -28,22 +40,26 @@ public class IssueControllerTest {
     @MockBean
     private IssueService issueService;
 
+    private Utente utenteTest;
+
+    @BeforeEach
+    void setup() {
+        utenteTest = new Utente();
+    }
+
     @Test
     public void test01() throws Exception {
 
         // ARRANGE
-        Priorita priorita = null;
-        Stato stato = null;
-        Tipo tipo = null;
-        Utente utente = null;
 
         //ACT
-        given(issueService.recuperaTutteLeIssues(priorita, stato, tipo, utente)).willReturn(Collections.emptyList());
+        given(issueService.recuperaTutteLeIssues(null, null, null, null)).willReturn(Collections.emptyList());
 
         //ASSERT
         mockMvc.perform(get("/issues")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()); 
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
@@ -51,11 +67,46 @@ public class IssueControllerTest {
 
         // Testare con il recupero di una issue
 
-        // ARRANGE
+        //ARRANGE
+        Issue issueTest = creaIssueFinta(Priorita.ALTA, Stato.TODO, Tipo.BUG,null);
+               
+        // ACT
+        given(issueService.recuperaTutteLeIssues(Priorita.ALTA, Stato.TODO, Tipo.BUG, null))
+                .willReturn(List.of(issueTest));
 
-        //ACT
+        // ASSERT
+        mockMvc.perform(get("/issues")
+                .param("priorita", "ALTA")
+                .param("stato", "TODO")
+                .param("tipo", "BUG")
+                // non metto utente perchè è null
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].priorita").value("ALTA"))
+                .andExpect(jsonPath("$[0].stato").value("TODO"))
+                .andExpect(jsonPath("$[0].tipo").value("BUG"))
+                .andExpect(jsonPath("$[0].utente").value(nullValue()));     
+    }
 
-        //ASSERT
-      
+    // Metodo di supporto - Helper
+    private Issue creaIssueFinta(Priorita priorita, Stato stato, Tipo tipo,Utente utente) {
+
+        Issue issue = new Issue();
+        // Fisse
+        issue.setId(1);
+        issue.setTitolo("Titolo della Issue");
+        issue.setDescrizione("Descrizione della Issue");
+
+        // Variabili
+        issue.setPriorita(priorita);
+        issue.setStato(stato);
+        issue.setTipo(tipo);
+        issue.setUtente(utente);
+
+
+        return issue;
     }
 }
