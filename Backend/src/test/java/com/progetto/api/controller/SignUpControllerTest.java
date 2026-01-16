e29e9f19e953afe836d7521e9e18bed7ca03ed59
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-01-16 10:52:12 +0100

    Aggiunta la classe di Testing per il metodo registraUtente

---
+++ Backend/src/test/java/com/progetto/api/controller/SignUpControllerTest.java
@@ -0,0 +1,61 @@
+package com.progetto.api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.progetto.api.service.SignUpService;
+import com.progetto.enums.utente.RuoloUtente;
+import com.progetto.exception.AuthException;
+import com.progetto.models.RichiestaRegistrazione;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SignUpController.class)
+public class SignUpControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SignUpService signUpService;
+
+    // Serve a convertire gli oggetti Java in JSON per la richiesta HTTP
+    @Autowired
+    private ObjectMapper objectMapper; 
+
+    @Test
+    public void test01() throws Exception {
+
+        //ARRANGE
+        RichiestaRegistrazione nuovoUtente = new RichiestaRegistrazione();
+        nuovoUtente.setEmail("mario@email.com");
+        nuovoUtente.setPassword("password123");
+        nuovoUtente.setNome("Mario");
+        nuovoUtente.setCognome("Rossi");
+        nuovoUtente.setRuolo(RuoloUtente.ADMIN);
+
+
+        // Poiché il metodo del service è 'void', usiamo doNothing().
+        doNothing().when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente))) // Converte l'oggetto in JSON string
+                
+        // ASSERT
+                .andExpect(status().isOk()) 
+                .andExpect(content().string("User created"));
+    }
+
+    
+}
