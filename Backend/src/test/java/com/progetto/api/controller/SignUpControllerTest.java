c1f762190e5aaea385e817502981744613fcaf98
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-01-16 13:15:53 +0100

    Completata la suite di test per la creazione di un utente (da rivedere solo ultimo test)

--- Backend/src/test/java/com/progetto/api/controller/SignUpControllerTest.java
+++ Backend/src/test/java/com/progetto/api/controller/SignUpControllerTest.java
@@ -36,7 +36,7 @@ public class SignUpControllerTest {
     public void test01() throws Exception {
 
         //ARRANGE (refactor da fare, possibile code smell long parameter list)
-        RichiestaRegistrazione nuovoUtente = creaUtente("mario@email.com","Password123!","Mario","Rossi", RuoloUtente.ADMIN);
+        RichiestaRegistrazione nuovoUtente = creaUtente("Admin@admin.com","AdminAdmin1!","Mario","Rossi", RuoloUtente.ADMIN);
 
         // Poiché il metodo del service è 'void', usiamo doNothing().
         doNothing().when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
@@ -53,9 +53,207 @@ public class SignUpControllerTest {
 
     @Test
     public void test02() throws Exception {
+
+        //ARRANGE
+        RichiestaRegistrazione nuovoUtente = creaUtente("Sviluppatore1@gmail.it","Sviluppatore1!","Lucia","Verdi", RuoloUtente.SVILUPPATORE);
+
+        doNothing().when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isOk()) 
+                .andExpect(content().string("User created"));
+    }
+
+    @Test
+    public void test03() throws Exception {
+        // ARRANGE
+        // campo email vuoto
+        RichiestaRegistrazione nuovoUtente = creaUtente(null,"Sviluppatore2!","Marco","Bianchi", RuoloUtente.SVILUPPATORE);
+
+        doThrow(new AuthException())
+                .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isBadRequest()) // Ci aspettiamo 400 Bad Request
+                .andExpect(content().string("Utente non creato"));
+
+    }
+
+    @Test
+    public void test04() throws Exception {
+        // ARRANGE
+        // campo email malformato
+        RichiestaRegistrazione nuovoUtente = creaUtente("aaa@","Sviluppatore2!","Marco","Bianchi", RuoloUtente.SVILUPPATORE);
+
+        doThrow(new AuthException())
+                .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isBadRequest()) // Ci aspettiamo 400 Bad Request
+                .andExpect(content().string("Utente non creato"));
+
+    }
+
+    @Test
+    public void test05() throws Exception {
+        // ARRANGE
+        // campo email malformato
+        RichiestaRegistrazione nuovoUtente = creaUtente("Sviluppatore2@gmail.it",null,"Marco","Bianchi", RuoloUtente.SVILUPPATORE);
+
+        doThrow(new AuthException())
+                .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isBadRequest()) // Ci aspettiamo 400 Bad Request
+                .andExpect(content().string("Utente non creato"));
+
+    }
+
+    @Test
+    public void test06() throws Exception {
         // ARRANGE
         // password che non rispetta i requisiti
-        RichiestaRegistrazione nuovoUtente = creaUtente("luca@email.com","pass1","Luca","Verdi", RuoloUtente.SVILUPPATORE);
+        RichiestaRegistrazione nuovoUtente = creaUtente("Sviluppatore2@gmail.it","passw","Marco","Bianchi", RuoloUtente.SVILUPPATORE);
+
+        doThrow(new AuthException())
+                .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isBadRequest()) // Ci aspettiamo 400 Bad Request
+                .andExpect(content().string("Utente non creato"));
+
+    }
+
+    @Test
+    public void test07() throws Exception {
+        // ARRANGE
+        // campo nome vuoto
+        RichiestaRegistrazione nuovoUtente = creaUtente("Sviluppatore2@gmail.it","Sviluppatore2!",null,"Bianchi", RuoloUtente.SVILUPPATORE);
+
+        doThrow(new AuthException())
+                .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isBadRequest()) // Ci aspettiamo 400 Bad Request
+                .andExpect(content().string("Utente non creato"));
+
+    }
+
+    @Test
+    public void test08() throws Exception {
+        // ARRANGE
+        // campo nome malformato
+        RichiestaRegistrazione nuovoUtente = creaUtente("Sviluppatore2@gmail.it","Sviluppatore2!","@@@","Bianchi", RuoloUtente.ADMIN);
+
+        doThrow(new AuthException())
+                .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isBadRequest()) // Ci aspettiamo 400 Bad Request
+                .andExpect(content().string("Utente non creato"));
+
+    }
+
+    @Test
+    public void test09() throws Exception {
+        // ARRANGE
+        // campo cognome vuoto
+        RichiestaRegistrazione nuovoUtente = creaUtente("Sviluppatore2@gmail.it","Sviluppatore2!","Mario",null, RuoloUtente.ADMIN);
+
+        doThrow(new AuthException())
+                .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isBadRequest()) // Ci aspettiamo 400 Bad Request
+                .andExpect(content().string("Utente non creato"));
+
+    }
+
+    @Test
+    public void test10() throws Exception {
+        // ARRANGE
+        // campo cognome malformato
+        RichiestaRegistrazione nuovoUtente = creaUtente("Sviluppatore2@gmail.it","Sviluppatore2!","Mario","###", RuoloUtente.ADMIN);
+
+        doThrow(new AuthException())
+                .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isBadRequest()) // Ci aspettiamo 400 Bad Request
+                .andExpect(content().string("Utente non creato"));
+
+    }
+
+    @Test
+    public void test11() throws Exception {
+        // ARRANGE
+        // campo cognome malformato
+        RichiestaRegistrazione nuovoUtente = creaUtente("Sviluppatore3@gmail.it","Sviluppatore3!","Carlo","Mancini", null);
+
+        doThrow(new AuthException())
+                .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
+
+        //ACT
+        mockMvc.perform(post("/sign-up")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nuovoUtente)))
+                
+        // ASSERT
+                .andExpect(status().isBadRequest()) // Ci aspettiamo 400 Bad Request
+                .andExpect(content().string("Utente non creato"));
+
+    }
+
+    @Test
+    public void test12() throws Exception {
+        // ARRANGE
+        // campo cognome malformato
+        RichiestaRegistrazione nuovoUtente = creaUtente("Sviluppatore2@gmail.it","Sviluppatore2!","Mario","###", RuoloUtente.SVILUPPATORE);
 
         doThrow(new AuthException())
                 .when(signUpService).registraUtente(any(RichiestaRegistrazione.class));
